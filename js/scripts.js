775395e4bf5af1f81ad14660b3870ff5915ea4e1
David Quisenberry and Sarah Leahy <sarah@>
2017-01-10 11:58:34 -0800

    add dynamic image src through jquery

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,19 @@ $(document).ready(function(){
     $("#content p").text("There is a rooftop pool.");
     $(".jumbotron").text("What you didn't know");
     $("#content p").css('color', 'green');
+    $("#content img").attr('src', 'img/roofpool.jpg');
   });
   $("#quickly").click(function(){
     $("#content").show();
     $("#content p").text("There are lots of food carts nearby");
     $("#content p").css('color', 'red');
+    $("#content img").attr('src', 'img/foodcart.jpg');
   });
   $("#pair").click(function(){
     $("#content").show();
     $("#content p").text("Don't forget your tic-tacs");
     $("#content p").css('color', 'blue');
+    $("#content img").attr('src', 'img/tictacs.png');
   });
   // $( "button:first" ).click(function() {
   //   $( "p:first" ).fadeToggle( "slow", "linear" );
