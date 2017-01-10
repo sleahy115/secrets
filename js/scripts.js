1fdb9af759723b3f7c5b4a0d4d7d658b1d77a019
David Quisenberry and Sarah Leahy <sarah@>
2017-01-10 11:25:17 -0800

    add show/hide

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $("#learn").click(function(){
+    $("#learn-content").show();
+  });
+  $("#quickly").click(function(){
+    $("#quickly-content").show();
+  });
+  $("#pair").click(function(){
+    $("#pair-content").show();
+  });
+  // $( "button:first" ).click(function() {
+  //   $( "p:first" ).fadeToggle( "slow", "linear" );
+  // });
+  // $( "button:last" ).click(function() {
+  //   $( "p:last" ).fadeToggle( "fast", function() {
+  //     $( "#log" ).append( "<div>finished</div>" );
+  //   });
+  // });
+  // $("#click").click(function(){
+  //   $("#whaleFade").fadeIn("slow");
+  // });
+  // $("#clickOut").click(function(){
+  //   $("#whaleFade").fadeOut("slow");
+  // });
+  // $(".clickable-fade-toggle").click(function(){
+  //   $("#toggle-walrus").fadeToggle();
+  //   $("#toggle-walrus-out").fadeToggle();
+  // });
+  // $(".clickable-slide").click(function(){
+  //   $("#slide-walrus").slideToggle();
+  //   $("#slide-walrus-out").slideToggle();
+  // });
+});
